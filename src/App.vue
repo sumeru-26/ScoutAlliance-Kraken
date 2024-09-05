fa1ae7bb6a9ca75e963898504dba484cc5fa463b
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-09-05 16:45:43 -0700

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+  import { RouterLink, RouterView } from 'vue-router'
+
+  import { useColorMode } from '@vueuse/core';
+
+  const mode = useColorMode()
+  mode.value = 'dark'
+
+</script>
+
+<template>
+  <RouterView />
+</template>
